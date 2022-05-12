935c8d23dee8aa2ded9a5d6c44a2e75d6c7dff85
Zork <yana_zorkaya2@mail.ru>
2022-05-12 18:07:49 +0300

    del Footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Route, Routes } from 'react-router-dom';
 import BrandPage from './pages/Brand';
 import Home from './pages/Home';
 import Header from './components/header';
-//import Footer from './components/footer';
 import FirebaseApp from './api/implementation/firebase/firebaseApp';
 
 function App() {
