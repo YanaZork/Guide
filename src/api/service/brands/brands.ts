889eb5698b4ce50792131773eeb657fd3deeee82
Zork <yana_zorkaya2@mail.ru>
2022-05-31 21:28:09 +0300

    all brend

--- src/api/service/brands/brands.ts
+++ src/api/service/brands/brands.ts
@@ -1,12 +1,14 @@
 
-import { collection, getDocs, setDoc, doc, updateDoc, query, orderBy, limit } from "firebase/firestore";
+import { collection, getDocs, setDoc, doc, updateDoc, query, orderBy } from "firebase/firestore";
+// import { limit } from "firebase/firestore";
 import { Brand } from "../../../types/Brand";
 import { Model } from "../../../types/Model";
 import database from '../../implementation/firestore/firestoreDatabase';
 
 export const getBrands = async (): Promise<Brand[]> => {
     let brands: Brand[] = [];
-    const q = query(collection(database(), "brand"), orderBy("name", "asc"), limit(3));
+// const q = query(collection(database(), "brand"), orderBy("name", "asc"), limit(30));
+    const q = query(collection(database(), "brand"), orderBy("name", "asc"));
     const querySnapshot = await getDocs(q);
     querySnapshot.forEach((doc) => {
         brands.push(doc.data() as Brand);
